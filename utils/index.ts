bdc62bfa33093f080ebb6a7320a70e03225d4f07
William Sanabria <william_sanabria@yahoo.es>
2023-08-10 21:38:34 -0500

    carcarddetail-creation

--- utils/index.ts
+++ utils/index.ts
@@ -1,5 +1,5 @@
 export async function fetchCars() {
-  const url = 'https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=corolla';
+  const url = 'https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=carrera';
   const options = {
     method: 'GET',
     headers: {
